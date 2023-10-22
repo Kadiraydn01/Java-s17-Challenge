43d1ae5b4d928dffc508b99bc243f66cfce35bc0
Kadiraydn01 <kadiraydn01@gmail.com>
2023-10-22 17:56:31 +0300

    Change

--- src/main/java/com/workintech/controller/CourseController.java
+++ src/main/java/com/workintech/controller/CourseController.java
@@ -16,66 +16,73 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/course")
 public class CourseController {
+
     private List<Course> courses;
     private CourseGpa low;
     private CourseGpa medium;
     private CourseGpa high;
 
     @PostConstruct
-    public void init(){
+    public void init() {
         courses = new ArrayList<>();
-
     }
 
-    @GetMapping("/")
-    public List<Course> findAll(){
-        return courses;
-    }
-
-    @GetMapping("/{name}")
-    public Course find(@PathVariable String name){
-        Optional<Course> optionalCourse = courses.stream().filter(course -> course.getName().equals(name)).findFirst();
-        if (optionalCourse.isPresent()){
-            return optionalCourse.get();
-        }else {
-            throw new CourseException("Course with given name is not exist: " + name, HttpStatus.NOT_FOUND);
-        }
-    }
-@Autowired
+    @Autowired
     public CourseController(@Qualifier("lowCourseGpa") CourseGpa low,
-                            @Qualifier ("mediumCourseGpa") CourseGpa medium,
-                            @Qualifier ("highCourseGpa") CourseGpa high) {
+                            @Qualifier("mediumCourseGpa") CourseGpa medium,
+                            @Qualifier("highCourseGpa") CourseGpa high) {
         this.low = low;
         this.medium = medium;
         this.high = high;
     }
 
+    @GetMapping("/")
+    public List<Course> findAll() {
+        return courses;
+    }
+
+    @GetMapping("/{name}")
+    public Course find(@PathVariable String name) {
+        Optional<Course> optionalCourse = courses.stream()
+                .filter(course -> course.getName().equals(name)).findFirst();
+        if (optionalCourse.isPresent()) {
+            return optionalCourse.get();
+        } else {
+            throw new CourseException("Course with given name is not exist: " + name, HttpStatus.NOT_FOUND);
+        }
+    }
+
     @PostMapping("/")
-    public CourseResponse save(@RequestBody Course course){
-        CourseValidation.checkCourseIsValid(course);
+    public CourseResponse save(@RequestBody Course course) {
         CourseValidation.isIdValid(course.getId());
+        CourseValidation.checkCourseIsValid(course);
         CourseValidation.isDuplicateNameFound(courses, course.getName());
         courses.add(course);
-        return CourseResponseFactory.createCourseResponse(course,low,medium,high);
+        return CourseResponseFactory.createCourseResponse(course, low, medium, high);
     }
+
     @PutMapping("/{id}")
-    public Course update(@RequestBody Course course,@PathVariable int id){
+    public Course update(@RequestBody Course course, @PathVariable int id) {
+        //[1, 2, 3, 4]
         CourseValidation.checkCourseIsValid(course);
-        Optional<Course> optionalCourse = courses.stream().filter(c -> c.getId() == id).findFirst();
-        if (optionalCourse.isPresent()){
+        Optional<Course> optionalCourse = courses.stream()
+                .filter(c -> c.getId() == id).findFirst();
+        if (optionalCourse.isPresent()) {
             int index = courses.indexOf(optionalCourse.get());
             course.setId(id);
-            courses.set(index,course);
+            courses.set(index, course);
             return course;
-        }else {
+        } else {
             throw new CourseException("Course with given id is not exist: " + id, HttpStatus.NOT_FOUND);
         }
     }
-@DeleteMapping("/{id}")
-    public Course delete(@PathVariable int id){
+
+    @DeleteMapping("/{id}")
+    public Course delete(@PathVariable int id) {
         Optional<Course> optionalCourse = courses.stream()
                 .filter(c -> c.getId() == id).findFirst();
         if (optionalCourse.isPresent()) {
@@ -87,4 +94,3 @@ public class CourseController {
         }
     }
 }
-
